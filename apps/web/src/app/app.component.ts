57b4f973cdc150cc073610cffc9d3fdb9fed3647
Jan Nicklas <jan.nicklas@tuta.io>
2023-12-27 15:53:08 +0100

    chore: setup ng-zorro component library

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [NzButtonModule, RouterModule],
   selector: 'co-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
